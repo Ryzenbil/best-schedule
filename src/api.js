c4403bccaa60097b0a0df74bed231d0089748931
Admin <ngrebenniko599@gmail.com>
2026-05-31 20:02:17 +0500

    changed url

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 // api.js
-const API_URL = 'http://localhost:8080/graphql'
+const API_URL = `${import.meta.env.VITE_API_URL}/graphql`
 
 export async function graphqlRequest(query, variables = {}) {
   const token = localStorage.getItem('token')
